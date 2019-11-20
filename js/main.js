cb8a6e1d3815199064d2f1c80e3a7c1b8df7fd14
b0br0o <edizol98@gmail.com>
2019-11-20 20:23:19 +0200

    slider added

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('.reviews-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true,
+    });
+
+
+
+
+
+
+});
