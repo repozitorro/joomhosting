a160d005c8e1c65bfc2162cb9d6d0b2cc63182d1
b0br0o <edizol98@gmail.com>
2019-11-21 22:09:35 +0200

    Adapted to various devices

--- js/main.js
+++ js/main.js
@@ -5,11 +5,18 @@ $(function(){
         slidesToScroll: 2,
         arrows: false,
         dots: true,
+        responsive: [
+            {
+              breakpoint: 1106,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+        ]
     });
 
-
-
-
-
-
+    $('.btn__menu').on('click' , function() {
+        $('.menu__list').slideToggle();
+    });
 });
